6c28e782d804ca626da1fded9d8cce4a19bd8156
heikeDaw <81535319+heikeDaw@users.noreply.github.com>
2023-11-23 08:46:09 +0100

    Update pantallaRegistro.php

--- app/manejadoresForm/pantallaRegistro.php
+++ app/manejadoresForm/pantallaRegistro.php
@@ -13,6 +13,10 @@ $fecha;
 $foto;
 $idioma;
 $descripcion;
+/*
+    Estos datos mejor en config.php
+*/
+
 $formatosFoto = ["jpg","png","gif"];
 $dirServidor = "../ficheros/imagenes";
 $dir = 'C:\xampp\htdocs\dwes2\app\ficheros\imagenes';
@@ -29,6 +33,9 @@ if(!isset($_REQUEST['bRegistro'])){
     $contrasenya = recoge("contrasenya");
     $fecha = recoge("fecha");
     $foto = $_FILES['foto'];
+    /* 
+        Los check mejor crearlos como array porque los manejas mejor. Además había que creaarlos dnamicamete en el form
+    */
     $espanyol = recoge("espanyol");
     $ingles = recoge("ingles");
     $valenciano = recoge("valenciano");
@@ -50,12 +57,21 @@ if(!isset($_REQUEST['bRegistro'])){
         $errores['contrasenya'] = "Es obligatorio introducir contraseña";
     }
     //TODO VALIDAR LA FECHA
+    /* 
+    Esta llamada da error porque faltan parámetros en la llamada
+    La propia función escribe en el array de errores
+    */
+    
     if(!validateDate($fecha)){
         $errores['fecha'] = "La fecha es obligatoria y debe cumplir el siguiente formato: DD/MM/YYYY.";
     }
 
     //TODO: Revisar los warnings que estan dando.
-
+/***
+    Subimos la foto sólo sino hay errores en el resto de los campos.
+    Tenemos que almacenar el resultado de cFile para saber el nombre con el que se ha guardado la imagen
+****/
+        
     cFile('foto', $errores,$formatosFoto, $dir,$maxFileSize); 
 
     //Validamos los input del checkbox idioma
@@ -70,12 +86,19 @@ if(!isset($_REQUEST['bRegistro'])){
     if ($archivo = fopen($rutaCompleta, "a")) {
         $hoy = date("Y-m-d H:i:s");
         $imagen;
+        /****
+        Puede haber un error con la imagen, tamaño demasiado grande, extensiones, ...
+        ****/
         if ($foto['name'] != ""){
            $imagen = $dirServidor."/".$foto['name'];
         }
         //TODO: ¿En la foto guardamos la ruta.?
         $usuario = "$nombre;$email;$contrasenya;$fecha;$idioma;$imagen;$descripcion;$hoy".PHP_EOL;
         if (fwrite($archivo, $usuario)){
+/****
+            Si luego vas a hacer un header location este echo no se mostrará
+***/
+            
             echo "Usuario Guardado correctamente.";
         }else{
             $errores['guardado'] = "Problema al realizar el guardado.";
@@ -87,6 +110,10 @@ if(!isset($_REQUEST['bRegistro'])){
 
     if(empty($errores)){
         //Redirigimos a la pagina de inicio de sesión
+        /*** 
+            Los echo antes de un header no se muestran
+        ****/
+            
         echo "Sin errores.";
         header("location:pantallaInicioSesion.php");
     } else{
